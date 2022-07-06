27b987e74066bb9d9c7c09689728403f2a05c745
BASSEM <bassem.jadoui@esprit.tn>
2022-07-07 00:51:19 +0100

    initilize project & customer microservice created ✅

---
+++ customer/src/main/java/com/fardi/customer/CustomerApplication.java
@@ -0,0 +1,12 @@
+package com.fardi.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
