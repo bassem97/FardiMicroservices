e433cfcba90db36dfef52813c29f6758fad780fb
BASSEM <bassem.jadoui@esprit.tn>
2022-07-23 15:03:08 +0100

    Entity ✅
    Took 4 minutes

---
+++ customer/src/main/java/com/fardi/customer/entity/Customer.java
@@ -0,0 +1,19 @@
+package com.fardi.customer.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Customers") @Data
+public class Customer {
+
+    @Id
+    public String id;
+
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String password;
+    public String phone;
+
+}
